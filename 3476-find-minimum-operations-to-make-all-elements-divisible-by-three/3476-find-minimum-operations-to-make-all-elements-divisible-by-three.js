9fb450f00c2884c241fa9984692dd2a08c2a41f7
shibimathew <shibi.l@ahalia.ac.in>
2025-04-29 10:25:17 +0530

    Time: 0 ms (100%), Space: 49.7 MB (100%) - LeetHub

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/3476-find-minimum-operations-to-make-all-elements-divisible-by-three.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations = function(nums) {
+    let count=0;
+for(let i=0;i<nums.length;i++){
+    if(nums[i]%3!==0){
+        count++
+    }
+}
+
+return count;
+    
+};
