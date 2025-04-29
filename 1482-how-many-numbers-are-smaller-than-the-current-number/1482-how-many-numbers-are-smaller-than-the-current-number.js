92ce6c96ad453b09e17033ad3f9e0ff1e499772b
shibimathew <shibi.l@ahalia.ac.in>
2025-04-29 10:26:01 +0530

    Time: 12 ms (20.65%), Space: 52.1 MB (100%) - LeetHub

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+    let result=[];
+    
+    for(let i=0;i<nums.length;i++){
+         let count=0;
+     for(let j=0;j<nums.length;j++){
+        if(j!==i && nums[j]<nums[i]){
+            count++
+        }
+     }
+     result.push(count)
+    }
+    return result;
+    
+};
