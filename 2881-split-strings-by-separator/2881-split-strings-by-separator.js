befad76c196cf3e0b641df55419f66f8fa4070f2
shibimathew <shibi.l@ahalia.ac.in>
2025-05-04 15:48:34 +0530

    Time: 4 ms (96.33%), Space: 61.8 MB (60.37%) - LeetHub

---
+++ 2881-split-strings-by-separator/2881-split-strings-by-separator.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string[]} words
+ * @param {character} separator
+ * @return {string[]}
+ */
+var splitWordsBySeparator = function(words, separator) {
+    
+    return words.join(separator).split(separator).filter(Boolean)
+};
