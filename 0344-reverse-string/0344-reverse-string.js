46212bf7ddb5e507081a78d84bfd5a0e648b8cc9
shibimathew <shibi.l@ahalia.ac.in>
2025-05-04 13:14:53 +0530

    Time: 0 ms (100%), Space: 61.9 MB (28.25%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,11 +3,5 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-let  start =0;
-let end=s.length-1
-while(start<end){
-    [s[start],s[end]]=[s[end],s[start]]
-    start++
-    end--
-}return s
+return s.reverse()
 };
