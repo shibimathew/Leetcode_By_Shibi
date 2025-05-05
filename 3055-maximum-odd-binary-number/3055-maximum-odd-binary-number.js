dfd18595c112ed453aa557bc29eb349fb310c993
shibimathew <shibi.l@ahalia.ac.in>
2025-05-05 11:52:39 +0530

    Time: 1 ms (89.84%), Space: 55.8 MB (57.22%) - LeetHub

--- 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.js
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.js
@@ -3,10 +3,7 @@
  * @return {string}
  */
 var maximumOddBinaryNumber = function(s) {
-      let ones = s.split('1').length - 1;  
-    let zeros = s.length - ones;         
-    
-    return '1'.repeat(ones - 1) + '0'.repeat(zeros) + '1';
-    
-    
+let ones = s.split('1').length - 1;
+let zero=s.length-ones
+return "1".repeat(ones-1)+"0".repeat(zero)+"1"
 };
