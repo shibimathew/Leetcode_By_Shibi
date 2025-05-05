d0dbd8c2695b6c37dd7b386a310201ff772dfb9c
shibimathew <shibi.l@ahalia.ac.in>
2025-05-05 12:20:51 +0530

    Time: 4 ms (15.23%), Space: 57.5 MB (77.67%) - LeetHub

---
+++ 1241-decompress-run-length-encoded-list/1241-decompress-run-length-encoded-list.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var decompressRLElist = function(nums) {
+    let result=[]
+    for(let i=0;i<nums.length;i+=2){
+        let freq=nums[i]
+        let val=nums[i+1]
+    
+      for(let j=0;j<freq;j++){
+        result.push(val);
+      }}
+      return result
+};
