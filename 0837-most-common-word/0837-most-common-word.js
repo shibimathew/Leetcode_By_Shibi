9e39e00a9973d7e9f21e8dc0bd66a11dc345f6e8
shibimathew <shibi.l@ahalia.ac.in>
2025-07-04 10:14:35 +0530

    Time: 6 ms (37.53%), Space: 56.4 MB (45.11%) - LeetHub

---
+++ 0837-most-common-word/0837-most-common-word.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function(paragraph, banned) {
+   
+   let para = paragraph.toLowerCase().replace(/[^a-z]/g, ' ') .split(/\s+/);
+
+    let count = {};
+    for (let i = 0; i < para.length; i++) {
+        let word = para[i];
+        if (word && !banned.includes(word)) {
+            count[word] = (count[word] || 0) + 1;
+        }
+    }
+       let maxWord = '';
+    let maxCount = 0;
+    for (let word in count) {
+        if (count[word] > maxCount) {
+            maxCount = count[word];
+            maxWord = word;
+        }
+    }return maxWord
+     
+  
+};
