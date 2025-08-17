69dc4c61e1063a1c4a87c6d5f456ca1c5e023c49
shibimathew <shibi.l@ahalia.ac.in>
2025-08-17 15:06:27 +0530

    Time: 0 ms (100%), Space: 53.2 MB (83%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.js
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.js
@@ -3,15 +3,22 @@
  * @return {number}
  */
 function largestAltitude(gain) {
-    let currentAltitude = 0;
-    let maxAltitude = 0;
+    // let currentAltitude = 0;
+    // let maxAltitude = 0;
 
-    for (let i = 0; i < gain.length; i++) {
-        currentAltitude += gain[i];
-        if (currentAltitude > maxAltitude) {
-            maxAltitude = currentAltitude;
-        }
+    // for (let i = 0; i < gain.length; i++) {
+    //     currentAltitude += gain[i];
+    //     if (currentAltitude > maxAltitude) {
+    //         maxAltitude = currentAltitude;
+    //     }
+    // }
+
+    // return maxAltitude;
+    let curr=0
+    let maxi=0
+    for(let i=0;i<gain.length;i++){
+        curr+=gain[i]
+        maxi= Math.max(maxi,curr)
     }
-
-    return maxAltitude;
+    return maxi
 }
