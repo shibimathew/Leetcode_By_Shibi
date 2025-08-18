3eff44397d29dc3f49a424a4349df014dbadf31f
shibimathew <shibi.l@ahalia.ac.in>
2025-08-18 15:16:16 +0530

    Time: 13 ms (9.13%), Space: 59.2 MB (81.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -6,11 +6,11 @@ var moveZeroes = function(nums) {
      let count = nums.length; 
     for(let i=0;i<count;i++){
         
-        if(nums[i]==0){
-            nums.splice(i,1)
-            nums.push(0)
-            count--
-            i--
-        }
+       if(nums[i]==0){
+        nums.splice(i,1)
+        nums.push(0)
+        count--
+        i--
+       }
     }return nums
 };
