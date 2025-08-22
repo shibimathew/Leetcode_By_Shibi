25431e19815202c11fcae260c2b98c69e9f63eb5
shibimathew <shibi.l@ahalia.ac.in>
2025-08-22 12:08:37 +0530

    Time: 12 ms (29.74%), Space: 59.2 MB (18.05%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,21 +3,13 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-  let arr=[1,1,2]
-  let arr1=[]
-for(i=0;i<nums.length;i++){
-    let count=0
-    for(j=0;j<nums.length;j++){
-        if(nums[i]==nums[j]){
-            count++
-        }
-    }
-    if(count==1){
-       arr1.push(nums[i]) 
-    }
-    
-    }
-    console.log(arr1)
-    return parseInt(arr1)
+let obj={}
+for(let i of nums){
+        obj[i]=(obj[i] || 0)+1
+}
+for(let i in obj){
+if(obj[i]===1){
+    return Number(i)
+}}
 
 };
