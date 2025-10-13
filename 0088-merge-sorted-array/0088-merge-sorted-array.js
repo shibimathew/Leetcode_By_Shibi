96dfc28bd0a7cc41c32d93c8df2eb3f5d6bf731c
shibimathew <shibi.l@ahalia.ac.in>
2025-10-13 10:25:39 +0530

    Time: 0 ms (100%), Space: 54 MB (89.5%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    nums1.length=m
+    for(let i=0;i<n;i++){
+        nums1.push(nums2[i])
+      
+    }
+   return nums1.sort((a,b)=>a-b)
+};
