f830df3e1f5b4a497b03c5dbe07fb63e8f9591ec
shibimathew <shibi.l@ahalia.ac.in>
2025-10-22 10:56:48 +0530

    Time: 0 ms (100%), Space: 54.1 MB (97.53%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.js
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.js
@@ -4,15 +4,12 @@
  * @return {number}
  */
 var countPairs = function(nums, target) {
-       let count=0
+    let count =0
     for(let i=0;i<nums.length;i++){
-        for(let j=0;j<nums.length;j++){
-            if(i<j && nums[i]+ nums[j]<target){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]<target){
                 count++
             }
         }
-
-}
-   return count; 
-    
+    }return count
 };
