25e2db109d98e03b49c301f7560c3e16132c8d07
shibimathew <shibi.l@ahalia.ac.in>
2025-10-22 12:37:51 +0530

    Time: 25 ms (11.08%), Space: 57.4 MB (9.87%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    for (let i = 0; i <= x; i++) {
-        if (i * i > x) {
-            return i - 1; // return the floor of sqrt
+    let a;
+    for(let i=0;i<=x;i++){
+        if(i*i>x){
+            return i-1
         }
-    }
-    return x; // covers x = 0 or 1
+    }return x
 };
