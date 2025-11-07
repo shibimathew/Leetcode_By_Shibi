5d31f81f45833e422cb87980a28f103d6ce2695a
shibimathew <shibi.l@ahalia.ac.in>
2025-11-07 06:44:27 +0530

    Time: 1 ms (87.9%), Space: 56.6 MB (46.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let a=[]
-    for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                a.push(i,j)
-            }
-        }
-    }return a
-
+   let map=new Map()
+   for(let i=0;i<nums.length;i++){
+   let c=target-nums[i]
+   if(map.has(c)){
+    return [map.get(c),i]
+   }
+  map.set(nums[i],i)
+   }
     
 };
