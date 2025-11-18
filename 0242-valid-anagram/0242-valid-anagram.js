3f4d3c23810505cc43d52ef221435ec5ed3dc8b9
shibimathew <shibi.l@ahalia.ac.in>
2025-11-18 11:40:26 +0530

    Time: 12 ms (71.92%), Space: 60.4 MB (20.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,5 +4,14 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-   return s.split('').sort().join("")==t.split('').sort().join("")
+    if(s.length!==t.length) return false
+    let h={}
+    for(let i of t){
+        h[i]=(h[i]||0)+1
+    }
+    for(let j of s){
+        if(!h[j])return false
+        h[j]--
+    }
+    return true
 };
