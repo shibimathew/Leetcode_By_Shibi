284119d4b0194d132c009e34062a31273ac410d0
shibimathew <shibi.l@ahalia.ac.in>
2025-11-18 10:16:21 +0530

    Time: 0 ms (100%), Space: 54.3 MB (42.98%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,9 +3,8 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let arr=s.trim().split(' ')
-    let c=arr[arr.length-1]
-    return c.split('').length
+     let arr = s.trim().split(' ');
+    return arr[arr.length - 1].length;
 
 
 };
