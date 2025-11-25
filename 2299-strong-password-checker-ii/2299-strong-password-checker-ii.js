2f620d31dced3eaade96a7c2787189fc9cc3f3da
shibimathew <shibi.l@ahalia.ac.in>
2025-11-25 14:38:34 +0530

    Time: 0 ms (100%), Space: 54.7 MB (38.71%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} password
+ * @return {boolean}
+ */
+var strongPasswordCheckerII = function(password) {
+    if(password.length<8)return false
+   
+      if(!password.match(/[a-z]/)){
+        return false
+      }
+       if(!password.match(/[0-9]/)){
+        return false
+      }
+       if(!password.match(/[A-Z]/)){
+        return false
+      }
+      if(!password.match(/[!@#$%^&*()\-\+]/)){
+      return false
+      }
+      if(password.match(/(.)\1/)){
+        return false
+      }
+    
+        return true
+    
+};
