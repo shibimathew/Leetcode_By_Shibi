6b00b5f5f938447c88c2051ac7e42af0813673ad
shibimathew <shibi.l@ahalia.ac.in>
2025-12-19 12:33:15 +0530

    Time: 1 ms (54.3%), Space: 55.7 MB (75.57%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.js
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.js
@@ -3,15 +3,13 @@
  * @return {boolean}
  */
 var canAliceWin = function(nums) {
-    let doubledigit =0;
-    let singlesum=0;
-for(let i=0;i<nums.length;i++){
-
-  if(nums[i]<10){
-       singlesum +=nums[i]
-  }else{
-  doubledigit +=nums[i]
-  }
+    let s=0;
+    let d=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]<10){
+            s+=nums[i]
+        }else{
+            d+=nums[i]
+        }
+    }return s!==d
 }
- return doubledigit!==singlesum
-};
