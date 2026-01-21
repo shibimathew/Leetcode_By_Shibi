719653b340e7770822f68b3f6db6c8270bb22101
shibimathew <shibi.l@ahalia.ac.in>
2026-01-21 11:59:50 +0530

    Time: 3 ms (25.03%), Space: 54.6 MB (80.09%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,14 +4,14 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-   let set1 = new Set(nums1);
-    let res = new Set();
-    
-    for (let num of nums2) {
-        if (set1.has(num)) {
-            res.add(num); 
-        }
-    } 
-    return Array.from(res);
-    
+    let arr=[]
+    for(let i=0;i<nums1.length;i++){
+        for(let j=0;j<nums2.length;j++){
+      if(nums1[i]===nums2[j] && !arr.includes(nums1[i])){
+        arr.push(nums1[i])
+        
+      }
+    }
+    }
+   return arr
 };
