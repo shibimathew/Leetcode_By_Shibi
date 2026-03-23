a4f6807742068866ddc5f0231cb81081951b7db0
shibimathew <shibi.l@ahalia.ac.in>
2026-03-23 13:58:40 +0530

    Time: 3 ms (38.85%), Space: 56.6 MB (58.6%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.js
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.js
@@ -4,12 +4,12 @@
  */
 var removeTrailingZeros = function(num) {
     let a=num.split('')
-  for(let i=a.length-1;i>=0;i--){
-    if(a[i]==='0'){
+   for(let i=a.length-1;i>=0;i--){
+    if(a[i]=='0'){
         a.pop()
     }else{
         break
     }
-  }
-  return a.join('')
+   }
+   return a.join('')
 };
