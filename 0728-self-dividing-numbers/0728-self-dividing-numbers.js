37f6a53207420d1d6f60f6d61e7a9c85251fc38f
shibimathew <shibi.l@ahalia.ac.in>
2026-03-23 11:26:56 +0530

    Time: 11 ms (15.22%), Space: 56.1 MB (38.41%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+    let arr=[]
+    for(let i=left;i<=right;i++){
+        let a=i.toString()
+        let valid=true
+        for(let j of a){
+            if(j==0||i%j!==0){
+               valid=false
+            }
+            
+        }
+        if(valid){
+            arr.push(i)
+        }
+    }return arr
+};
