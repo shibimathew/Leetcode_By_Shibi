4d3673f2c982067595475df76537020758b8c93c
shibimathew <shibi.l@ahalia.ac.in>
2026-03-25 00:30:59 +0530

    Time: 0 ms (100%), Space: 53 MB (92.21%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,26 +4,11 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    let a=0
-    let flag=0
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]==target){
-         a=i;
-         flag++;
-        }
+ for(let i=0;i<nums.length;i++){
+    if(target<=nums[i]){
+        return i
+       
     }
-    if(flag!=0){
-        return a
-    }else{
-        nums.push(target)
-        nums.sort((a,b)=>a-b)
-        for(let j=0;j<nums.length;j++){
-          if(nums[j]==target){
-            return j
-          }
-    }
-    }
-
-    
-    
+ } 
+ return nums.length  
 };
