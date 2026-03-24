2de2afe88e057aad2934dbc74b56554bf5719de6
shibimathew <shibi.l@ahalia.ac.in>
2026-03-25 01:17:33 +0530

    Time: 0 ms (100%), Space: 56.7 MB (32.61%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let count=0
+    for(let i=0;i<hours.length;i++){
+        if(target<=hours[i]){
+            count++
+        }
+    }return count
+};
