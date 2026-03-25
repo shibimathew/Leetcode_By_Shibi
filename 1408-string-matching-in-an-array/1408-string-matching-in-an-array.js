ff37f2f17c6a48fe6964502476806273d2a6c9b8
shibimathew <shibi.l@ahalia.ac.in>
2026-03-25 13:14:40 +0530

    Time: 3 ms (47.94%), Space: 53.4 MB (89.69%) - LeetHub

---
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var stringMatching = function(words) {
+    let res=[]
+    for(let i=0;i<words.length;i++){
+        for(let j=0;j<words.length;j++){
+            if(i!==j && words[j].includes(words[i])){
+                res.push(words[i])
+                break;
+            }
+        }
+    }return res
+};
