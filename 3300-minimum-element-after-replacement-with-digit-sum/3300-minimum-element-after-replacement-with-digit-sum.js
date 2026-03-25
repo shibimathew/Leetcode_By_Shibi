3fa99bdae22fc8274586a4ccb700ebc1240947e6
shibimathew <shibi.l@ahalia.ac.in>
2026-03-25 11:37:44 +0530

    Time: 2 ms (62.22%), Space: 55.4 MB (89.63%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.js
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var minElement = function(nums) {
- 
-  let min=Infinity
-   for(let i=0;i<nums.length;i++){
-     let sum=0
-      let s=nums[i].toString()
-      for(let j=0;j<s.length;j++){
+
+    let mini=Infinity
+for(let i=0;i<nums.length;i++){
+    let s=nums[i].toString()
+        let sum=0
+    for(let j=0;j<s.length;j++){
         sum+=Number(s[j])
-      
-      }
-      min=Math.min(min,sum)
-   }
-   return min 
+    }
+    mini=Math.min(mini,sum)
+}
+return mini
 };
