e00da879daa830b20ce64c6aebceca62a152352a
shibimathew <shibi.l@ahalia.ac.in>
2026-06-09 08:11:53 +0530

    Time: 1 ms (98.91%), Space: 69 MB (53.53%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+        let sum = 0;
+
+    // First window
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    let maxSum = sum;
+
+    // Slide the window
+    for (let i = k; i < nums.length; i++) {
+        sum = sum + nums[i] - nums[i - k];
+        if (sum > maxSum) {
+            maxSum = sum;
+        }
+    }
+
+    return maxSum / k;
+};
