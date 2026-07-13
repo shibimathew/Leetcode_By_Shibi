2c1e864d316febd077090f759f1d4db1a991bb92
shibimathew <shibi.l@ahalia.ac.in>
2026-07-13 21:28:26 +0530

    Time: 0 ms (100%), Space: 53.6 MB (67.12%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,10 +4,11 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]==val){
-           nums.splice(i,1)
-           i--
-        }
-    }return nums.length
+    let k=0
+   for(let i=0;i<nums.length;i++){
+    if(nums[i]!==val){
+        nums[k]=nums[i]
+        k++
+    }
+   }return k
 };
